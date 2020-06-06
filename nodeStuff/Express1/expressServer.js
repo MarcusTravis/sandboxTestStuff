1870b97fadc39eb17d7737d75376003604a7fa81
Marcus <mpt789@gmail.com>
2020-06-05 18:21:06 -0700

    update for udemy

--- nodeStuff/Express1/expressServer.js
+++ nodeStuff/Express1/expressServer.js
@@ -1,6 +1,8 @@
 // NODEJS IS the language
 // Express is node, a node module
 
+const path = require("path");
+
 // http is a native module
 // const http = require('http');
 // express is a 3rd party module
@@ -9,14 +11,22 @@ const express = require('express');
 // invoked and is an Express application
 const app = express();
 
+app.use(express.static("public"));
+
 // all is a method, and it takes 2 args:
 // 1. route
 // 2. callback to run if the route is requested
-app.all('*',(req, res)=>{
+app.all('/',(req, res)=>{
     // Express handles the basic headers (status code, mime-type)! Awesome!
-    res.send(`<h1>This is the home page</h1>`)
+    // read in Node.html
+    console.log(path.join(__dirname + "/node.html"));
+    res.sendFile(path.join(__dirname + "/node.html"));
+    // res.send(`<h1>This is the home page</h1>`)
     // Express handles the end! Awesome!
 })
+app.all("*", (req, res) => {
+    res.send("<h1>This page does not exist.</h1>");
+});
 
 app.listen(3000);
 console.log("The server is listening on port 3000.");
